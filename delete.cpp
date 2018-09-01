e73fb27b75c3900485a9217c26772a87de8eaa10
roman990 <42899435+roman990@users.noreply.github.com>
2018-09-02 03:18:50 +0530

    Add files via upload

---
+++ delete.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+	int a1,a2,b1,b2,c;
+	int text,i,sub;
+			
+			cin>>a1>>b1;
+			cin>>a2>>b2;
+			
+			c=a2-a1;
+			b1=b1+c;	
+		 
+		 
+			if(b1==b2)
+		{
+			cout<<"diag element";
+		}
+		
+			
+			
+			
+			
+
+
+}	
